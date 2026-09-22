82b34e596ecfe91b2fe4e5a32ea3f1bb66f2c8ba
satyasheel Gautam <143704829+satyasheel87@users.noreply.github.com>
2026-09-22 13:39:48 +0530

    Time: 4 ms (32.98%), Space: 56.6 MB (32.23%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+    nums = Array.from(new Set(nums))
+    nums.sort((a, b) => b - a)
+    if (nums.length >= 3) {
+        return nums[2]
+    }
+    return nums[0]
+};

57c5f257ebbd86452bbfbb825575cc59c2d338bc
satyasheel Gautam <143704829+satyasheel87@users.noreply.github.com>
2026-09-23 23:58:42 +0530

    Time: 0 ms (100%), Space: 52.9 MB (85.43%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    let j = 0;
+    let count = 0
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[j] = nums[i];
-            j++;
+            nums[count] = nums[i]
+            count++
         }
     }
-    return j;
+    return count
 };
